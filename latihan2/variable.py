59e0fbe330de5731d9915976eb8f5531c5489f42
NovanR <novanramdan7@gmail.com>
2023-12-09 09:12:44 +0700

    lanjutan latihan

---
+++ latihan2/variable.py
@@ -0,0 +1,8 @@
+a = 10
+b = 5
+print ("Nilai A adalah = ",a)
+print ("Nilai B adalah = ",b)
+print ("Hasil dari penjumlahan nya adalah", a+b)
+
+a = 7
+print ("Nilai A berubah menjadi ",a)
---
+++ latihan3/tipedata.py
@@ -0,0 +1,19 @@
+ini_integer = 10
+print ("Tipe data dari",ini_integer,"adalah", type(ini_integer))
+
+ini_float = 3.14
+print("Tipe data dari",ini_float,"adalah", type(ini_float))
+
+ini_string = "Novan Ramdan"
+print("Tipe data dari",ini_string,"adalah", type(ini_string))
+
+ini_boolean = True
+print("Tipe data dari",ini_boolean,"adalah", type(ini_boolean))
+
+ini_complex = complex(5,6)
+print("Tipe data dari",ini_complex,"adalah", type(ini_complex))
+
+# tipe data dari bahasa C
+from ctypes import c_double
+data_c_double = c_double(3.14)
+print("Tipe data dari",data_c_double,"adalah", type(data_c_double))
